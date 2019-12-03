e2463dcabfe275964aa67401967aeb0df1c3455b
hejiawang <hejiawang521>
2019-12-03 20:35:25 +0800

    init

---
+++ src/main/java/com/wang/jmonkey/modules/ieg/model/dto/IegReportDetailDto.java
@@ -0,0 +1,56 @@
+package com.wang.jmonkey.modules.ieg.model.dto;
+
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchool;
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchoolDetail;
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchoolFaculty;
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchoolProblem;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * @Description: IegReportDetailDto
+ * @Auther: heJiawang
+ * @Date: 2019-12-03
+ */
+@Data
+@Accessors(chain = true)
+public class IegReportDetailDto {
+
+    /**
+     * 院校基本信息
+     */
+    private IegSchool school;
+
+    /**
+     * 院校详细信息
+     */
+    private IegSchoolDetail schoolDetail;
+
+    /**
+     * 院校特征
+     */
+    private List<String> featureList;
+
+    /**
+     * 院校所在地气候信息
+     */
+    private String environment;
+
+    /**
+     * 院校常见问题汇总
+     */
+    private List<IegSchoolProblem> problemList;
+
+    /**
+     * 院系信息
+     */
+    private List<IegSchoolFaculty> facultyList;
+
+    /**
+     * 院校专业信息
+     */
+    private List<IegReportMajorDto> majorList;
+
+}
---
+++ src/main/java/com/wang/jmonkey/modules/ieg/model/dto/IegReportMajorDto.java
@@ -0,0 +1,33 @@
+package com.wang.jmonkey.modules.ieg.model.dto;
+
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchoolMajor;
+import com.wang.jmonkey.modules.ieg.model.entity.IegSchoolMajorProblem;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * @Description: IegReportMajorDto
+ * @Auther: heJiawang
+ * @Date: 2019-12-03
+ */
+@Data
+@Accessors(chain = true)
+public class IegReportMajorDto extends IegSchoolMajor {
+
+    /**
+     * 专业特征名称
+     */
+    private List<String> featureNames;
+
+    /**
+     * 所属院系名称
+     */
+    private String facultyName;
+
+    /**
+     * 问题汇总
+     */
+    private List<IegSchoolMajorProblem> problemList;
+}
